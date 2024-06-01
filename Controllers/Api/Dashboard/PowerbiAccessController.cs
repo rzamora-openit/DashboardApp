36b7bbe3012bcea41693323dd9200a98aa1cc189
rzamora-openit <rzamora.openit@gmail.com>
2024-06-01 08:17:48 +0800

    initial commit .Net 8

--- Controllers/Api/Dashboard/PowerbiAccessController.cs
+++ Controllers/Api/Dashboard/PowerbiAccessController.cs
@@ -1,5 +1,4 @@
-﻿using AutoMapper;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OpeniT.PowerbiDashboardApp.Data;
 using OpeniT.PowerbiDashboardApp.Helpers;
--- Program.cs
+++ Program.cs
@@ -1,26 +1,111 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.CookiePolicy;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Newtonsoft.Json;
+using OpeniT.PowerbiDashboardApp.Data;
+using OpeniT.PowerbiDashboardApp.Helpers;
+using OpeniT.PowerbiDashboardApp.Models.Accounts;
 
 namespace OpeniT.PowerbiDashboardApp
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			CreateHostBuilder(args).Build().Run();
-		}
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var services = builder.Services;
 
-		public static IHostBuilder CreateHostBuilder(string[] args) =>
-			Host.CreateDefaultBuilder(args)
-				.ConfigureWebHostDefaults(webBuilder =>
-				{
-					webBuilder.UseStartup<Startup>();
-				});
-	}
+            services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DataConnection")), ServiceLifetime.Transient);
+
+            services.AddTransient<ApplicationLogger>();
+            services.AddTransient<DataRepository>();
+
+            services.AddSingleton<AzureQueryHelper>();
+            services.AddSingleton<AzureQueryRepository>();
+
+            services.AddSingleton<PowerBIQueryHelper>();
+            services.AddSingleton<PowerBIQueryRepository>();
+            services.AddScoped<PowerBIEmbedHelper>();
+
+            services.AddAuthentication()
+               .AddOpenIdConnect(options =>
+               {
+                   options.Authority = builder.Configuration["Microsoft:Authority"];
+                   options.ClientId = builder.Configuration["Microsoft:ClientId"];
+                   options.SignedOutCallbackPath = new PathString("/signout-callback-oidc");
+               });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Internal", policy => policy.RequireAssertion(Security.Assertions.IsInternal));
+            });
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+               .AddEntityFrameworkStores<DataContext>()
+               .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.AccessDeniedPath = new PathString("/NotFound");
+            });
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+
+                options.HttpOnly = HttpOnlyPolicy.Always;
+                options.Secure = CookieSecurePolicy.Always;
+            });
+
+            services
+              .AddControllersWithViews(options =>
+              {
+                  var policy = new AuthorizationPolicyBuilder()
+                      .RequireAuthenticatedUser()
+                      .Build();
+                  options.Filters.Add(new AuthorizeFilter(policy));
+              })
+              .AddNewtonsoftJson(options =>
+              {
+                  options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+              });
+
+            services.AddHttpContextAccessor();
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseCookiePolicy();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(name: "areaRoute", pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
+            app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
 }
